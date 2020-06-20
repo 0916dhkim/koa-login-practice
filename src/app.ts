ce4d26fd106b03f256b4255c1c5ed255c5a0061b
Donghyeon Kim <0916dhkim@gmail.com>
2020-06-20 15:41:19 +0900

    Serve Static Files

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import * as Koa from "koa";
+import * as serve from "koa-static";
+import { resolve } from "path";
 const app = new Koa();
 
-app.use(ctx => {
-    ctx.body = "Hello, World!";
-});
+app.use(serve(resolve("static")));
+app.use(serve(resolve("dist")));
 
 app.listen(process.env.PORT);
