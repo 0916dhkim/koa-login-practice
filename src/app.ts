87deb5d02ed2cb621ff7f39e0ad200a229c20dc1
Donghyeon Kim <0916dhkim@gmail.com>
2020-06-20 16:11:39 +0900

    Add Login Endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import * as Koa from "koa";
 import * as serve from "koa-static";
+import * as Router from "@koa/router";
+import * as bodyParser from "koa-bodyparser";
 import { resolve } from "path";
-const app = new Koa();
 
+const app = new Koa();
+const router = new Router();
+
+// Handle login.
+router.post("/login", (ctx, next) => {
+    console.table(ctx.request.body);
+});
+
+app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());
 app.use(serve(resolve("static")));
 app.use(serve(resolve("dist")));
 
